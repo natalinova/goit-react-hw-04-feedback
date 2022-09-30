204e816ea21ad398eff42ea50b159075b5c35122
Nataliya Novak <novak.natali@gmail.com>
2022-09-30 21:26:00 +0300

    finally

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,42 +1,48 @@
-import { Component } from "react";
+import { useState } from "react";
 import FeedbackBlock from "./FeedbackBlock";
 import Statistics from "./Statistics";
 import FeedbackOptions from "./FeedbackOptions";
 
-
-export default class Feedback extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad:0
+export default function Feedback() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+   
+    const addFeedback = (nameFeedback) => {
+        switch (nameFeedback) {
+            case 'good':
+                setGood(good + 1);
+                break;
+            case 'neutral':
+                setNeutral(neutral + 1);
+                break;
+            case 'bad':
+                setBad(bad + 1);
+                break;
+            default : {return }     
+        }
     }
-    addFeedback = (nameFeedback) => {
-        this.setState((prevState) => {
-            const value = prevState[nameFeedback];
-            return { [nameFeedback]: value + 1}
-    })
-    }
-    countTotalFeedback() {
-        const { good, neutral, bad } = this.state;
+        
+   const  countTotalFeedback = () => {
+       
         return good + neutral + bad;
     }
-    countPositiveFeedbackPercentage() {
-        const total = this.countTotalFeedback();
+    const countPositiveFeedbackPercentage = () => {
+        const total = countTotalFeedback();
         if (!total) {
             return 0
         }
-        const value = this.state.good;
+        const value = good;
         const result = ( value/ total) * 100;
         return Number(result.toFixed(2))
     
     }
-    render() {
-        const { good, neutral, bad } = this.state;
-        const totalFeedback = this.countTotalFeedback();
-        const positiveFeedback = this.countPositiveFeedbackPercentage();
+    
+        const totalFeedback = countTotalFeedback();
+        const positiveFeedback = countPositiveFeedbackPercentage();
         return (
             <FeedbackBlock title='Please, leave feedback!'>
-                <Statistics addFeedback={this.addFeedback} />
+                <Statistics addFeedback={addFeedback} />
                 {!totalFeedback ? <p>There is no feedback</p> :
                     <FeedbackOptions
                         good={good}
@@ -47,9 +53,54 @@ export default class Feedback extends Component {
                     />}
         
             </FeedbackBlock>
+        )
+}
+
+// export default class Feedback extends Component {
+//     state = {
+//         good: 0,
+//         neutral: 0,
+//         bad:0
+//     }
+//     addFeedback = (nameFeedback) => {
+//         this.setState((prevState) => {
+//             const value = prevState[nameFeedback];
+//             return { [nameFeedback]: value + 1}
+//     })
+//     }
+//     countTotalFeedback() {
+//         const { good, neutral, bad } = this.state;
+//         return good + neutral + bad;
+//     }
+//     countPositiveFeedbackPercentage() {
+//         const total = this.countTotalFeedback();
+//         if (!total) {
+//             return 0
+//         }
+//         const value = this.state.good;
+//         const result = ( value/ total) * 100;
+//         return Number(result.toFixed(2))
+    
+//     }
+//     render() {
+//         const { good, neutral, bad } = this.state;
+//         const totalFeedback = this.countTotalFeedback();
+//         const positiveFeedback = this.countPositiveFeedbackPercentage();
+//         return (
+//             <FeedbackBlock title='Please, leave feedback!'>
+//                 <Statistics addFeedback={this.addFeedback} />
+//                 {!totalFeedback ? <p>There is no feedback</p> :
+//                     <FeedbackOptions
+//                         good={good}
+//                         neutral={neutral}
+//                         bad={bad}
+//                         totalFeedback={totalFeedback}
+//                         positiveFeedback={positiveFeedback}
+//                     />}
+        
+//             </FeedbackBlock>
            
 
-        )
-    }
+//         )
+//     }
 
-}
